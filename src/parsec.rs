7b0a33cc4e46478d81d5c649a5007eaaab4846a5
Roland Clairquin <roland.clairquin@aeronomie.be>
2018-08-11 22:28:31 +0200

    Parse as a struct + implementation of methods seq and bind

--- src/parsec.rs
+++ src/parsec.rs
@@ -1,40 +1,74 @@
-pub type Parser<T> = Box<Fn(&str) -> Vec<(T, &str)>>;
+pub type ParserFn<T> = Box<Fn(&str) -> Vec<(T, &str)>>;
+pub struct Parser<T> {
+    parse_fn: ParserFn<T>,
+}
 
+// 3 primitive parsers ---------------------------------------------------
 pub fn result<T>(v: T) -> Parser<T>
 where
     T: 'static + Copy,
 {
-    Box::new(move |inp| vec![(v, inp)])
+    Parser::new(Box::new(move |inp| vec![(v, inp)]))
 }
 
 pub fn zero() -> Parser<i32> {
-    Box::new(|_| vec![])
+    Parser::new(Box::new(|_| vec![]))
 }
 
 pub fn item() -> Parser<char> {
-    Box::new(|inp| {
+    Parser::new(Box::new(|inp| {
         let mut chars = inp.chars();
         match chars.next() {
             Some(c) => vec![(c, chars.as_str())],
             None => vec![],
         }
-    })
+    }))
 }
 
-pub fn seq<T, U>(p: Parser<T>, q: Parser<U>) -> Parser<(T, U)>
+impl<T> Parser<T>
 where
     T: 'static + Copy,
-    U: 'static + Copy,
 {
-    Box::new(move |inp| {
-        let mut r = vec![];
-        for (v, inp1) in p(inp) {
-            for (w, inp2) in q(inp1) {
-                r.push(((v, w), inp2))
-            }
+    pub fn new(parse_fn: ParserFn<T>) -> Parser<T> {
+        Parser { parse_fn }
+    }
+
+    pub fn parse(self, inp: &str) -> Vec<(T, &str)> {
+        (self.parse_fn)(inp)
+    }
+
+    // Parser combinators ----------------------------------------------
+    pub fn seq<U>(self, other: Parser<U>) -> Parser<(T, U)>
+    where
+        U: 'static + Copy,
+    {
+        Parser {
+            parse_fn: (Box::new(move |inp| {
+                let mut r = vec![];
+                for (v, inp1) in (self.parse_fn)(inp) {
+                    for (w, inp2) in (other.parse_fn)(inp1) {
+                        r.push(((v, w), inp2));
+                    }
+                }
+                r
+            })),
         }
-        r
-    })
+    }
+
+    pub fn bind<U>(self, f: Box<Fn(T) -> Parser<U>>) -> Parser<U>
+    where
+        U: 'static + Copy,
+    {
+        Parser::new(Box::new(move |inp| {
+            let mut r = vec![];
+            for (v, inp1) in (self.parse_fn)(inp) {
+                for (w, inp2) in f(v).parse(inp1) {
+                    r.push((w, inp2));
+                }
+            }
+            r
+        }))
+    }
 }
 
 #[cfg(test)]
@@ -43,26 +77,34 @@ mod test {
 
     #[test]
     fn result_succeeds_without_consuming_input_string() {
-        assert_eq!(result(42)("brol"), vec![(42, "brol")]);
+        assert_eq!(result(42).parse("brol"), vec![(42, "brol")]);
     }
 
     #[test]
     fn zero_always_fails() {
-        assert_eq!(zero()("brol"), vec![]);
+        assert_eq!(zero().parse("brol"), vec![]);
     }
 
     #[test]
     fn item_consumes_the_first_char() {
-        assert_eq!(item()(""), vec![]);
-        assert_eq!(item()("a"), vec![('a', "")]);
-        assert_eq!(item()("↓"), vec![('↓', "")]);
-        assert_eq!(item()("brol"), vec![('b', "rol")]);
-        assert_eq!(item()("↓brol"), vec![('↓', "brol")]);
+        assert_eq!(item().parse(""), vec![]);
+        assert_eq!(item().parse("a"), vec![('a', "")]);
+        assert_eq!(item().parse("↓"), vec![('↓', "")]);
+        assert_eq!(item().parse("brol"), vec![('b', "rol")]);
+        assert_eq!(item().parse("↓brol"), vec![('↓', "brol")]);
     }
 
     #[test]
     fn seq_applies_one_parser_after_another() {
-        let two_items = seq(item(), item());
-        assert_eq!(two_items("brol"), [(('b', 'r'), "ol")]);
+        let two_items = item().seq(item());
+        assert_eq!(two_items.parse("brol"), [(('b', 'r'), "ol")]);
+    }
+
+    #[test]
+    fn bind_first_test() {
+        let two_items = item().bind(Box::new(|v| {
+            item()
+        }));
+        assert_eq!(two_items.parse("brol"), [('r', "ol")]);
     }
 }
